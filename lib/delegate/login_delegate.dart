064e821d6045f2b446f92a54427727c55363a471
Deivid Marreiro <davidteixeiro@gmail.com>
2020-06-24 23:00:42 -0300

    Thir commit Register Screen added

---
+++ lib/delegate/login_delegate.dart
@@ -0,0 +1,20 @@
+import 'package:agriculturapp/screens/login_screen.dart';
+import 'package:agriculturapp/screens/register_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginDelegate {
+  static mudarParaTelaDeRegistro(BuildContext context) {
+    Navigator.push(
+      context,
+      CupertinoPageRoute(builder: (context) => RegisterScreen()),
+    );
+  }
+
+  static mudarParaTelaDeLogin(BuildContext context) {
+    Navigator.pushReplacement(
+      context, 
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+}
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,14 +1,55 @@
 import 'package:agriculturapp/delegate/login_delegate.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
+class RegisterScreen  extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _RegisterScreenState createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
   bool _seLembre = false;
-  Widget _constroiEmail() {
+  Widget _constroiCadastroNome() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Nome Completo',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.black38,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 6.0,
+                  offset: Offset(0, 2),
+                ),
+              ]),
+          height: 60.0,
+          child: TextField(
+            keyboardType: TextInputType.text,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.person_outline,
+                color: Colors.white,
+              ),
+              hintText: 'Entre com seu Nome',
+              hintStyle: TextStyle(color: Colors.white54),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _constroiCadastroEmail() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -49,7 +90,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget _constroiSenha() {
+  Widget _constroiCadastroSenha() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -79,7 +120,7 @@ class _LoginScreenState extends State<LoginScreen> {
               border: InputBorder.none,
               contentPadding: EdgeInsets.only(top: 14.0),
               prefixIcon: Icon(
-                Icons.lock,
+                Icons.vpn_key,
                 color: Colors.white,
               ),
               hintText: 'Entre com sua Senha',
@@ -91,59 +132,61 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget _constroiEsqueceuSenha() {
-    return Container(
-      alignment: Alignment.centerRight,
-      child: FlatButton(
-        onPressed: () => print('Botao de esqueceu senha pressionado'),
-        padding: EdgeInsets.only(right: 0.0),
-        child: Text(
-          'Esqueceu sua senha?',
+  Widget _constroiCadastroSenhaConfirmar() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Confirmar Senha',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
-      ),
-    );
-  }
-
-  Widget _caixaLembreDeMim() {
-    return Container(
-      height: 20.0,
-      child: Row(
-        children: <Widget>[
-          Theme(
-            data: ThemeData(unselectedWidgetColor: Colors.black),
-            child: Checkbox(
-              value: _seLembre,
-              checkColor: Colors.green,
-              activeColor: Colors.black,
-              onChanged: (value) {
-                setState(() {
-                  _seLembre = value;
-                });
-              },
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.black38,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 6.0,
+                  offset: Offset(0, 2),
+                ),
+              ]),
+          height: 60.0,
+          child: TextField(
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.vpn_key,
+                color: Colors.white,
+              ),
+              hintText: 'Confirme sua senha',
+              hintStyle: TextStyle(color: Colors.white54),
             ),
           ),
-          Text('Lembre de mim',
-              style:
-                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
-  Widget _botaoDeEntrar() {
+  Widget _botaoDeRegistrar() {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 25.0),
       width: double.infinity,
       child: RaisedButton(
         elevation: 5.0,
-        onPressed: () => print('Botão de login apertado'),
+        onPressed: () => print('Botão de registro apertado'),
         padding: EdgeInsets.all(15.0),
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
         color: Colors.white,
         child: Text(
-          'ENTRAR',
+          'REGISTRAR',
           style: TextStyle(
             color: Color(0xFF00C853),
             letterSpacing: 2.5,
@@ -157,18 +200,18 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget _botaoRegistrar() {
     return GestureDetector(
-      onTap: () => LoginDelegate.mudarParaTelaDeRegistro(context),
+      onTap: () => LoginDelegate.mudarParaTelaDeLogin(context),
       child: RichText(
         text: TextSpan(children: [
           TextSpan(
-              text: 'Não tem uma conta? ',
+              text: 'Já tem uma conta? ',
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 18.0,
                 fontWeight: FontWeight.w400,
               )),
           TextSpan(
-              text: 'Registrar-se ',
+              text: 'Efetue o Login ',
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 18.0,
@@ -213,7 +256,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'Entrar',
+                    'Registrar-se',
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 40.0,
@@ -221,17 +264,16 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   SizedBox(height: 30.0),
-                  _constroiEmail(),
+                  _constroiCadastroNome(),
+                  SizedBox(height: 15.0),
+                  _constroiCadastroEmail(),
+                  SizedBox(height: 15.0),
+                  _constroiCadastroSenha(),
+                  //_caixaLembreDeMim(),
+                  SizedBox(height: 15.0),
+                  _constroiCadastroSenhaConfirmar(),
                   SizedBox(height: 30.0),
-                  _constroiSenha(),
-                  _constroiEsqueceuSenha(),
-                  _caixaLembreDeMim(),
-                  _botaoDeEntrar(),
-                  Container(
-                      child: Text(
-                    ' - OU - ',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )),
+                  _botaoDeRegistrar(),
                   SizedBox(height: 20.0),
                   _botaoRegistrar(),
                 ],
